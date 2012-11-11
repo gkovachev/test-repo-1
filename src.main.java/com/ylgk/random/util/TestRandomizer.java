d89dba1cc956584c1ebde6330f728d6b34e6cfdf
gkovachev <gkovachev@tacitknowledge.com>
2012-11-11 15:14:02 -0800

    Second commit: added TestRandomizer and a corresponding test

---
+++ src.main.java/com/ylgk/random/util/TestRandomizer.java
@@ -0,0 +1,9 @@
+package com.ylgk.random.util;
+
+public class TestRandomizer {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Test Randomizer");
+	}
+
+}
---
+++ src.test.java/com/ylgk/random/util/RandomizerTest.java
@@ -0,0 +1,19 @@
+package com.ylgk.random.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RandomizerTest {
+
+	@Test
+	public void test() {
+		
+		try {
+			System.out.println("Pass this test for now.");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+	}
+}
